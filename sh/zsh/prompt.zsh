11e73af81038e24ff30db782a40fa27b6d0ec024
llgtr <llgtr@users.noreply.github.com>
2018-04-25 03:15:59 +0300

    Update zsh prompt
    - Use different prompt when in a ssh connection
    - Update git dirty command

--- sh/zsh/prompt.zsh
+++ sh/zsh/prompt.zsh
@@ -1,12 +1,12 @@
 git_dirty() {
     command git rev-parse --is-inside-work-tree &>/dev/null || return
 
-    command git diff --quiet --ignore-submodules HEAD &>/dev/null;
-    if [[ $? -eq 1 ]]; then
-        echo " %F{red}(*)%f"
-    else
-        echo " %F{green}(-)%f"
-    fi
+    #command git diff --quiet --ignore-submodules HEAD &>/dev/null;
+    command test -z "$(command git status --porcelain --ignore-submodules -unormal)"
+
+    [[ $? -eq 1 ]] && echo " %F{red}(*)%f" && return
+
+    echo " %F{green}(-)%f"
 }
 
 git_branch() {
@@ -17,12 +17,20 @@ git_branch() {
 
 function zle-line-init zle-keymap-select {
     V_P="%F{green}[N]%f"
-    export RPROMPT='${${KEYMAP/vicmd/$V_P}/(main|viins)/}`git_branch``git_dirty`'
+    RPROMPT="${${KEYMAP/vicmd/$V_P}/(main|viins)/}`git_branch``git_dirty`"
     zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
 
-export PROMPT='[%~]: '
+prompt_setup() {
+    DEF_PROMPT="[%~]: "
+    SSH_PROMPT="%F{245}(%F{003}%n%F{245}@%F{016}%m%F{245})%f ${DEF_PROMPT}"
 
+    [[ -n $SSH_CONNECTION ]] && PROMPT="${SSH_PROMPT}" && return
+
+    PROMPT="${DEF_PROMPT}"
+}
+
+prompt_setup
