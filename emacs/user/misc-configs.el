244f61736bdb466f17c5ae0e6e852d2f70f8ed53
llgtr <llgtr@users.noreply.github.com>
2019-12-24 14:30:23 +0200

    emacs: Improve reading envfile;
    * Check envfile for possible errors
    * Remove unnecessary intermediate steps
    * Rename let bindings for better clarity

--- emacs/user/misc-configs.el
+++ emacs/user/misc-configs.el
@@ -58,33 +58,38 @@
 ;; Prompt y/n instead of yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(defvar env-file (expand-file-name "envfile" user-emacs-directory))
+
 ;; Helper function that reads envfile created by emacs-env.sh. Mostly copied
 ;; from doom-emacs.
-;; TODO: Clean this up
-(defun read-env-file ()
-  (let ((env-file (expand-file-name "envfile" user-emacs-directory)))
-    (if (file-exists-p env-file)
-        (with-temp-buffer
-          (insert-file-contents env-file)
-          (while (re-search-forward "\n *\\([^#= \n]*\\)=" nil t)
-            (let ((envvars (match-string 1))
-                  (environment (buffer-substring
+(defun read-user-env-file ()
+  (if (file-exists-p env-file)
+      (with-temp-buffer
+        (insert-file-contents env-file)
+        (while (re-search-forward "\n *\\([^#= \n]*\\)=" nil t)
+          (let* ((env-var-val (split-string
+                               (buffer-substring
                                 (match-beginning 1)
                                 (- (or (save-excursion
                                          (when (re-search-forward "^\\([^= ]+\\)=" nil t)
                                            (line-beginning-position)))
-                                       (point-max)) 1))))
-              (let* ((variable-and-value (split-string environment "="))
-                     (env-var (car variable-and-value))
-                     (env-val (car (cdr variable-and-value))))
-                (setenv env-var env-val)
-                (if (string-equal "PATH" envvars)
-                    (setq exec-path (append (parse-colon-path (getenv "PATH"))
-                                            (list exec-directory))))
-                (if (string-equal "SHELL" envvars)
-                    (setq shell-file-name
-                          (or (getenv "SHELL") shell-file-name)))))))
-      (error (format "%s does not exist. Run emacs-env.sh" env-file)))))
+                                       (point-max)) 1))
+                               "="))
+                 (env-var (car env-var-val))
+                 (env-val (cadr env-var-val)))
+            (if (and (eq (length env-var-val) 2)
+                     (not (seq-empty-p env-val))
+                     (not (string-match "\n" env-val)))
+                (progn
+                  (setenv env-var env-val)
+                  (if (string-equal "PATH" env-var)
+                      (setq exec-path (append (parse-colon-path (getenv "PATH"))
+                                              (list exec-directory))))
+                  (if (string-equal "SHELL" env-var)
+                      (setq shell-file-name
+                            (or (getenv "SHELL") shell-file-name))))
+              (error (format "%s is malformed. Run `emacs-env.sh` and overwrite the envfile." env-file))))))
+    (error (format "%s does not exist. Run `emacs-env.sh`." env-file))))
 
 ;; MacOS things
 (when (eq system-type 'darwin)
@@ -92,6 +97,6 @@
   (setq mac-right-option-modifier nil)
   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
   (add-to-list 'default-frame-alist '(ns-appearance . dark))
-  (read-env-file))
+  (read-user-env-file))
 
 (provide 'misc-configs)
