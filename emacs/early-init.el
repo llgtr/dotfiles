305424f378cab4a03c056ee6d878b7dfe98e124b
llgtr <llgtr@users.noreply.github.com>
2020-03-05 09:06:30 +0200

    emacs: Optimize startup further;
    * Use early-init.el to move optimizations closer to startup

---
+++ emacs/early-init.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+
+;; Set garbage collection
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 20 1024 1024))))
+
+(setq package-enable-at-startup nil)
+(advice-add #'package--ensure-init-file :override #'ignore)
+
+;; Turn these off early to avoid seeing them at all
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Inhibit frame resizing when font size is different from system default
+(setq frame-inhibit-implied-resize t)
+
--- emacs/init.el
+++ emacs/init.el
@@ -2,21 +2,8 @@
 
 ;;; init.el -- emacs meets vim
 
-;; Turn these off early to avoid seeing them at all
-(scroll-bar-mode 0)
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-
-;; Set garbage collection
-(setq gc-cons-threshold (* 128 1024 1024))
-(add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 20 1024 1024))))
-
 ;; Path to user directory
 (add-to-list 'load-path (expand-file-name "user" user-emacs-directory))
-
-;; Inhibit frame resizing when font size is different from system default
-(setq frame-inhibit-implied-resize t)
 
 ;; Load configurations
 (require 'packages)
