2ff2d6b7a04eafa1d09e1677b41000058bbd7c01
llgtr <llgtr@users.noreply.github.com>
2020-08-20 17:22:11 +0300

    fonts: Rebuild clone layer if more than a day old

--- fonts/Iosevka-Custom/build.sh
+++ fonts/Iosevka-Custom/build.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 rm -rf $SCRIPT_DIR/ttf/
-docker build -t iosevka-docker $SCRIPT_DIR/docker/.
+docker build -t iosevka-docker --build-arg CACHEBUST=$(date +%Y%m%d) $SCRIPT_DIR/docker/.
 docker run --name iosevka-builder iosevka-docker
 docker cp iosevka-builder:/tmp/Iosevka/dist/iosevka-custom/ttf/ $SCRIPT_DIR/.
 docker stop iosevka-builder
