bce0ba958d99067a88c23037ee480425a0c89754
llgtr <llgtr@users.noreply.github.com>
2021-01-22 13:03:31 +0200

    emacs: Increase gc thresholds

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,9 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
 ;; Set garbage collection
-(setq gc-cons-threshold (* 128 1024 1024))
+(setq gc-cons-threshold (* 256 1024 1024))
 (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 64 1024 1024))))
+          (lambda () (setq gc-cons-threshold (* 128 1024 1024))))
 
 (setq package-enable-at-startup nil)
 (advice-add #'package--ensure-init-file :override #'ignore)
