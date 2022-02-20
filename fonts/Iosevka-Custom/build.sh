2e5e339364f917cdfc3478114bb26ce8ec54b8f8
llgtr <llgtr@users.noreply.github.com>
2022-02-20 13:21:56 +0200

    fonts: Use podman for build container

--- fonts/Iosevka-Custom/build.sh
+++ fonts/Iosevka-Custom/build.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-if ! docker info > /dev/null 2>&1; then
-    echo "Please ensure that Docker is running"
+if ! podman info > /dev/null 2>&1; then
+    echo "Please ensure that podman machine is running"
     exit 1
 fi
 
-docker build -t iosevka-docker --build-arg CACHEBUST=$(date +%Y%m%d) $SCRIPT_DIR/docker/.
-docker run --name iosevka-builder iosevka-docker
+podman build -t iosevka-container --build-arg CACHEBUST=$(date +%Y%m%d) $SCRIPT_DIR/plans/.
+podman run --name iosevka-builder iosevka-container
 rm -rf $SCRIPT_DIR/out/
-docker cp iosevka-builder:/tmp/Iosevka/dist/iosevka-custom/ $SCRIPT_DIR/out/
-docker stop iosevka-builder
-docker rm iosevka-builder
+podman cp iosevka-builder:/tmp/Iosevka/dist/iosevka-term/ $SCRIPT_DIR/out
+podman stop iosevka-builder
+podman rm iosevka-builder
