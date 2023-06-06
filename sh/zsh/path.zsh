fbc586d508585e21f8b337628b91f92758ab5895
llgtr <llgtr@users.noreply.github.com>
2023-06-06 13:23:22 +0300

    zsh: Remove flutter from PATH

--- sh/zsh/path.zsh
+++ sh/zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}:${HOME}/.local/bin:${HOME}/.cargo/bin:${HOME}/flutter/bin"
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}:${HOME}/.local/bin:${HOME}/.cargo/bin"
