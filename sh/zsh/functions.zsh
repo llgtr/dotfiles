1fa8aecfb201a4787b1ee975b10626c05fc5a3a8
llgtr <llgtr@users.noreply.github.com>
2024-03-19 13:39:09 +0200

    zsh: Implement function for organising files;
    * Ease friction of importing and organising photos for long-term storage

--- sh/zsh/functions.zsh
+++ sh/zsh/functions.zsh
@@ -41,3 +41,27 @@ function fu() {
         eval "$(fnm env)" && fnm use
     fi
 }
+
+# Find and extract files matching given extension from given directory
+# $1: File extension
+# $2: Source directory
+function extract_files() {
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "$(echo_c "ERROR:" 1 -b) Invalid parameters" >&2
+        return 1
+    fi
+
+    local FILE_EXTENSION="$1"
+    local SOURCE_DIR="$2"
+    local TARGET_DIR="${FILE_EXTENSION#.}"
+
+    echo "This will extract files from $(realpath $SOURCE_DIR)"
+    printf "%s" "$(echo_c "Are you sure [y/n]? " 3)"
+    read -r -n 1
+    mkdir -p "$TARGET_DIR"
+
+    find "$SOURCE_DIR" -type f -name "*$FILE_EXTENSION" | while read -r file; do
+        echo "Moving $file"
+        mv "$file" "$TARGET_DIR"
+    done
+}
