043077ad351e7a1a04c3bdaf6ee884040ea657c5
llgtr <llgtr@users.noreply.github.com>
2024-05-20 15:46:16 +0300

    mac: Remove nix

--- mac/nixpkgs/install_nix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-if [[ ! -e "/nix" ]]; then
-    sudo mkdir -m 0755 "/nix" && sudo chown "$(whoami)" "/nix"
-
-    curl "https://nixos.org/nix/install" | bash -s -- --no-daemon
-fi
